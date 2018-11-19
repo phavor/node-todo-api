bdcf51df60c5ff0d4a3dc76f72f47636d00eab65
Fayvor George <phavorsparks@gmail.com>
2018-11-19 22:02:35 +0100

    setup getbyid and update todo

--- controllers/todo.js
+++ controllers/todo.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Todo = require('../model/todo-schema')
 
 module.exports = {
+  // create new todo
   new_todo (req, res) {
     let todo = new Todo({
       text: req.body.text,
@@ -22,9 +23,42 @@ module.exports = {
       })
   },
 
-  get_todo (req, res) {
+  // get all todos
+  get_all_todos (req, res) {
     Todo.find()
       .select('-__v')
+      .then(doc => {
+        res.status(200).send(doc)
+      })
+      .catch(err => {
+        res.status(400).send(err.message)
+      })
+  },
+
+  // get todo by id
+  get_todo (req, res) {
+    const _id = req.params.id
+
+    Todo.findById(_id)
+      .select(' -__v')
+      .then(doc => {
+        res.status(200).send(doc)
+      })
+      .catch(err => {
+        res.status(400).send(err.message)
+      })
+  },
+
+  // update todo by id
+  update_todo (req, res) {
+    const id = req.params.id
+
+    Todo.findByIdAndUpdate(id, {
+      $set: {
+        text: req.body.text,
+        completed: req.body.completed
+      }
+    }, { new: true })
       .then(doc => {
         res.status(200).send(doc)
       })
--- routes/todo.js
+++ routes/todo.js
@@ -5,7 +5,8 @@ const router = express.Router()
 const todoController = require('../controllers/todo')
 
 router.post('/new_todo', todoController.new_todo)
-
-router.get('/', todoController.get_todo)
+router.get('/', todoController.get_all_todos)
+router.get('/:id', todoController.get_todo)
+router.patch('/:id', todoController.update_todo)
 
 module.exports = router
