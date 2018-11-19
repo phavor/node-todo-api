6aee314569b4735392aa4e217063f9e19fb46665
Fayvor George <phavorsparks@gmail.com>
2018-11-19 23:03:08 +0100

    update with token generation and user creation

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose')
+const _ = require('lodash')
 const User = require('../model/user-schema')
 
 module.exports = {
   new_user (req, res) {
-    let user = new User({
-      email: req.body.email,
-      username: req.body.username
-    })
+    const body = _.pick(req.body, ['email', 'username', 'password'])
+
+    let user = new User(body)
 
     user.save()
-      .then(doc => {
-        res.send(doc)
+      .then(userDoc => {
+        return user.generateAuthToken()
+      })
+      .then(token => {
+        res.status(201)
+          .header('x-auth', token)
+          .send(user)
       })
       .catch(err => {
-        res.status(500)
+        res.status(400)
         .json({
           message: 'Could not save user',
           error: err.message
--- model/user-schema.js
+++ model/user-schema.js
@@ -1,19 +1,59 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 8
+    minlength: 8,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
   username: {
     type: String,
     required: true,
     trim: true,
-    minlength: 3
-  }
+    minlength: 3,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
+
+UserSchema.methods.generateAuthToken = function () {
+  let user = this
+  let access = 'auth'
+  let token = jwt.sign({ _id: user._id.toHexString(), access }, 'process.env.SECRET_KEY').toString()
+
+  user.tokens.push({ access, token })
+  return user.save().then(() => token)
+}
+
+UserSchema.methods.toJSON = function () {
+  let user = this
+  let userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email', 'username'])
+}
 
 const User = mongoose.model('Users', UserSchema)
 
